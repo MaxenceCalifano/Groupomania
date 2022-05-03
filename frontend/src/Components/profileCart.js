aca95b58992020e1574ad6df047f54ba0016a542
Maxence <maxence.califano@gmail.com>
2022-05-03 08:35:43 +0200

    add arrow on pofile card

--- frontend/src/Components/profileCart.js
+++ frontend/src/Components/profileCart.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import {Link } from "react-router-dom";
 import '../css/profileCard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown} from '@fortawesome/free-solid-svg-icons';
 
 export default function Profile(props) {
     const [avatar, setavatar] = useState();
@@ -28,9 +30,11 @@ export default function Profile(props) {
                 onMouseEnter={() =>setIsHovered(true)}
                 onMouseLeave={() =>setIsHovered(false)}
                 onBlur={() =>setIsHovered(false)} 
-                > 
-            {avatar !== undefined ? <img className="avatar" src={`http://localhost:3001/images/${avatar}`} alt="avatar"></img> : ""}
-            
+                >
+            <div className="profileCard_notHovered">
+                <FontAwesomeIcon icon={faChevronDown}  className="profileCard_icon"/>
+                {avatar !== undefined ? <img className="avatar" src={`http://localhost:3001/images/${avatar}`} alt="avatar"></img> : ""}
+            </div>
                 <div className={isHovered ? "profileCard_options" : "profileCard_notDisplayed"}
                     >
                     <p>Votre profil :</p>
