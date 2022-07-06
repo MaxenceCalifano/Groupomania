cbf81d0022011d06d79d7877ce4ff1ee7df86b07
Maxence <maxence.califano@gmail.com>
2022-07-06 09:05:03 +0200

    change logs

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,8 +3,8 @@ dotenv.config()
 
 module.exports = 
 {
-  HOST: eu-cdbr-west-03.cleardb.net, //"localhost",
-  USER: process.env.dbUser,
-  PASSWORD: process.env.dbPassword,
-  DB: heroku_4a07fe7768e41a0, //"groupomania"
+  HOST: "eu-cdbr-west-03.cleardb.net", //"localhost",
+  USER: "bdfe45b6657637",
+  PASSWORD: "126fa972",
+  DB: "heroku_4a07fe7768e41a0", //"groupomania"
 }
