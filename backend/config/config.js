309e4b144fadb186b5e956bd622454980a00748e
MaxenceCalifano <48595795+MaxenceCalifano@users.noreply.github.com>
2022-07-06 09:24:15 +0200

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,8 +3,8 @@ dotenv.config()
 
 module.exports = 
 {
-  HOST: "eu-cdbr-west-03.cleardb.net", //"localhost",
+  HOST: "localhost",
   USER: "bdfe45b6657637",
   PASSWORD: "126fa972",
-  DB: "heroku_4a07fe7768e41a0", //"groupomania"
+  DB: "groupomania"
 }
